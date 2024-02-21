fdc3e52e8709924cbced647aa5c8f0428330dcf9
Ebons <eboagi@live.com>
2024-02-21 21:19:57 +0300

    adding quiz1.py file

---
+++ quiz1.py
@@ -0,0 +1,49 @@
+print("Welcome to my computer quiz! ")
+playing = input("Do you what to play ? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :) ")
+
+score = 0
+answer = input("what does CPU stand for ? ")
+if answer.lower() == "central processing unit":
+    print("You are correct!")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("what does GPU stand for ? ")
+if answer.lower() == "graphics processing unit":
+    print("You are correct!")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("what does RAM stand for ? ")
+if answer.lower() == "random access memory":
+    print("You are correct!")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("what does ROM stand for ? ")
+if answer.lower() == "read only memory":
+    print("You are correct!")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("what does PSU stand for ? ")
+if answer.lower() == "power supply unit":
+    print("You are correct!")
+    score += 1
+else:
+    print("Incorrect! ")
+
+print("You have got " + str(score) + " questions correct!")
+print("You have got ", ((score / 5) * 100), "%")
+
+
+
